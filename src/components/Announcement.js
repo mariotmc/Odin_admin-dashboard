331bf2296501b971b6b9333e7829c021e4587b3d
mariotmc <mariotomicx@gmail.com>
2022-03-08 16:39:47 +0100

    Create announcement component

---
+++ src/components/Announcement.js
@@ -0,0 +1,10 @@
+const Announcement = ({ heading }) => {
+  return (
+    <article className="announcement">
+      <h3>{heading}</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    </article>
+  );
+};
+
+export default Announcement;
